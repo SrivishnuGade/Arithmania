fc95809cdff37b76d6bc5d271f3d831a7396a6f2
Suchir <smv1524@gmail.com>
2024-10-29 10:04:05 +0530

    ground added

--- src/main.jsx
+++ src/main.jsx
@@ -40,7 +40,7 @@ scene.add(sunlight);
 
 // Updated Ground setup
 const planeGeometry = new THREE.PlaneGeometry(1000, 1000);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x808080 });
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x77dd77 });
 const ground = new THREE.Mesh(planeGeometry, planeMaterial);
 ground.rotation.x = -Math.PI / 2;
 ground.position.y = -1;
